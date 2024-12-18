63daca8f93c3ba35d35e64572e7543285e0b08a5
Chuck Daminato <chuck.daminato@recursionpharma.com>
2024-12-18 14:47:48 -0500

    Day 2 part 2
    Refactor slightly to have a re-usable checker, but add a loop for part 2
    which excludes an element each pass to determine safety

--- 2024/src/02.py
+++ 2024/src/02.py
@@ -12,19 +12,16 @@ input_str = get_input(2)
 # """
 
 # WRITE YOUR SOLUTION HERE
-safe = 0
-direction = 0
-for report in input_str.splitlines():
-  array = report.strip().split(" ")
 
-  array_len = len(array)
+# determine if an report is "safe"
+def is_report_safe(report):
+  array_len = len(report)
   for i in range(array_len):
     # reached the end without unsafe
     if i == array_len - 1:
-      safe += 1
-      break
+      return True
 
-    a, b = int(array[i]), int(array[(i + 1)])
+    a, b = int(report[i]), int(report[(i + 1)])
     # determine if we're ascending or descending, first elements only
     if i == 0:
       if a < b:
@@ -33,7 +30,33 @@ for report in input_str.splitlines():
         direction = -1
 
     # if we switch directions or the difference is more than 3 or zero, it's unsafe
-    if (b - a) * direction <= 0 or (b - a) * direction > 3: 
+    if (b - a) * direction <= 0 or (b - a) * direction > 3:
+      return False
+
+  return True
+
+safe = 0
+safe2 = 0
+for report in input_str.splitlines():
+  report_array = report.strip().split(" ")
+
+  array_len = len(report_array)
+  if is_report_safe(report_array):
+    safe += 1
+
+  # take a single element out of the report, one at a time, then do all the same things from here
+  is_safe = False
+  for i in range(len(report_array)):
+    # skip if this report has already been marked safe
+    if is_safe:
       break
+    report_array_copy = report_array.copy()
+    report_array_copy.pop(i) # remove each element one at a time
+
+    if is_report_safe(report_array_copy):
+      is_safe = True
+      safe2 += 1
+
 
 print("Safe: ", safe)
+print("Safe 2: ", safe2)
